49f9f8e43a27dc49f28dc2f00cd233012887f8f9
Andrew Davison <andrew.davison@cnrs.fr>
2022-11-04 15:00:59 +0100

    Add search/autocomplete to single-select drop-down lists

--- apps/model_catalog/src/SingleSelect.js
+++ apps/model_catalog/src/SingleSelect.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
-import ListItemText from "@material-ui/core/ListItemText";
-import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import { formatLabel } from "./utils";
 
@@ -41,53 +39,18 @@ export default function SingleSelect(props) {
     return (
         <div>
             <FormControl className={classes.formControl}>
-                <InputLabel id={fieldLabelId}>
-                    {formatLabel(props.label)}
-                </InputLabel>
-                <Select
+                <Autocomplete
                     labelId={fieldLabelId}
                     id={fieldId}
-                    value={props.value ? props.value : ""}
                     name={fieldName}
+                    options={props.itemNames}
+                    style={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label={formatLabel(props.label)} />}
                     onChange={props.handleChange}
-                    input={<Input />}
-                    MenuProps={MenuProps}
-                >
-                    {
-                        // conditional rendering syntax
-                        props.label !== "implementation_status" && (
-                            <MenuItem key="None" value="">
-                                <ListItemText primary="None" />
-                            </MenuItem>
-                        )
-                    }
-                    {props.itemNames.map((name) => (
-                        <MenuItem key={name} value={name}>
-                            <ListItemText primary={name} />
-                        </MenuItem>
-                    ))}
-                </Select>
+                />
                 <FormHelperText>{props.helperText}</FormHelperText>
             </FormControl>
         </div>
 
-        // Alternative (styling pending)
-        // <div>
-        //   <TextField
-        //     id={fieldId}
-        //     select
-        //     label={capitalize(props.label)}
-        //     value={props.value}
-        //     onChange={props.handleChange}
-        //     helperText={props.helperText}
-        //     variant="outlined"
-        //   >
-        //     {props.itemNames.map((name) => (
-        //       <MenuItem key={name} value={name} MenuProps={MenuProps}>
-        //         {name}
-        //       </MenuItem>
-        //     ))}
-        //   </TextField>
-        // </div>
     );
 }
