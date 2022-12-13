gara-elhaddad/model-catalog

commit 53357c3d0f3227c02335e644b1a206810ee35a24

move filtering of editable projects to the server

apps/model_catalog/src/datastore.js
@@ -353,17 +353,11 @@ class DataStore {
         if (this.projects.length > 0) {
             return this.projects;
         } else {
-            const url = this.baseUrl + "/projects";
+            const url = this.baseUrl + "/projects?only_editable=true";
             return this.get(url, source)
                 .then((res) => {
-                    let editableProjects = [];
-                    res.data.forEach((proj) => {
-                        if (proj.permissions.UPDATE) {
-                            editableProjects.push(proj.project_id);
-                        }
-                    });
-                    editableProjects.sort();
-                    this.projects = editableProjects;
+                    this.projects = res.data;
+                    console.log(this.projects);
                     return this.projects;
                 })
                 .catch((err) => {

commit 928b912cc710e9659a441bd41066bbedea4b019c

fix bug introduced in 53357c3

apps/model_catalog/src/datastore.js
@@ -356,8 +356,11 @@ class DataStore {
             const url = this.baseUrl + "/projects?only_editable=true";
             return this.get(url, source)
                 .then((res) => {
-                    this.projects = res.data;
-                    console.log(this.projects);
+                    let projectNames = [];
+                    res.data.forEach((proj) => {
+                        projectNames.push(proj.project_id);
+                    });
+                    this.projects = projectNames;
                     return this.projects;
                 })
                 .catch((err) => {