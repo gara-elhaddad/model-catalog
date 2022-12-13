gara-elhaddad/model-catalog

commit 90cf39d801166757a03c82f02948e056c7b5dd72

implement test and model duplicate feature

apps/model_catalog/src/ModelAddForm.js
@@ -55,28 +55,28 @@ export default class ModelAddForm extends React.Component {
             errorAddModel: null,
             isAliasNotUnique: true,
             aliasLoading: false,
-            name: "",
-            alias: "",
-            author: [],
-            owner: [],
-            private: false,
-            project_id: "",
-            description: "",
-            species: "",
-            brain_region: "",
-            cell_type: "",
-            model_scope: "",
-            abstraction_level: "",
-            organization: "",
+            name: this.props.duplicateData.name || "",
+            alias: this.props.duplicateData.alias || "",
+            author: this.props.duplicateData.author || [],
+            owner: this.props.duplicateData.owner || [],
+            private: this.props.duplicateData.private || false,
+            project_id: this.props.duplicateData.project_id || "",
+            description: this.props.duplicateData.description || "",
+            species: this.props.duplicateData.species || "",
+            brain_region: this.props.duplicateData.brain_region || "",
+            cell_type: this.props.duplicateData.cell_type || "",
+            model_scope: this.props.duplicateData.model_scope || "",
+            abstraction_level: this.props.duplicateData.abstraction_level || "",
+            organization: this.props.duplicateData.organization || "",
             instances: [
                 {
-                    version: "",
-                    description: "",
-                    parameters: "",
-                    morphology: "",
-                    source: "",
-                    code_format: "",
-                    license: "",
+                    version: this.props.duplicateData.instances[0].version || "",
+                    description: this.props.duplicateData.instances[0].description || "",
+                    parameters: this.props.duplicateData.instances[0].parameters || "",
+                    morphology: this.props.duplicateData.instances[0].morphology || "",
+                    source: this.props.duplicateData.instances[0].source || "",
+                    code_format: this.props.duplicateData.instances[0].code_format || "",
+                    license: this.props.duplicateData.instances[0].license || "",
                 },
             ],
             auth: authContext,

apps/model_catalog/src/TestAddForm.js
@@ -53,26 +53,26 @@ export default class TestAddForm extends React.Component {
             errorAddTest: null,
             isAliasNotUnique: true,
             aliasLoading: false,
-            name: "",
-            alias: "",
-            author: [],
-            description: "",
-            data_location: [],
-            data_type: "",
-            species: "",
-            brain_region: "",
-            cell_type: "",
-            test_type: "",
-            score_type: "",
-            recording_modality: "",
-            implementation_status: "",
+            name: this.props.duplicateData.name || "",
+            alias: this.props.duplicateData.alias || "",
+            author: this.props.duplicateData.author || [],
+            description: this.props.duplicateData.description || "",
+            data_location: this.props.duplicateData.data_location || [],
+            data_type: this.props.duplicateData.data_type || "",
+            species: this.props.duplicateData.species || "",
+            brain_region: this.props.duplicateData.brain_region || "",
+            cell_type: this.props.duplicateData.cell_type || "",
+            test_type: this.props.duplicateData.test_type || "",
+            score_type: this.props.duplicateData.score_type || "",
+            recording_modality: this.props.duplicateData.recording_modality || "",
+            implementation_status: this.props.duplicateData.implementation_status || "",
             instances: [
                 {
-                    version: "",
-                    repository: "",
-                    path: "",
-                    description: "",
-                    parameters: "",
+                    version: this.props.duplicateData.instances[0].version || "",
+                    repository: this.props.duplicateData.instances[0].repository || "",
+                    path: this.props.duplicateData.instances[0].path || "",
+                    description: this.props.duplicateData.instances[0].description || "",
+                    parameters: this.props.duplicateData.instances[0].parameters || "",
                 },
             ],
             auth: authContext,
@@ -90,7 +90,6 @@ export default class TestAddForm extends React.Component {
     }
 
     handleCancel() {
-        console.log("Hello");
         this.props.onClose();
     }
 
@@ -353,7 +352,7 @@ export default class TestAddForm extends React.Component {
                                     <TextField
                                         name="data_location"
                                         label="Data Location (URL)"
-                                        defaultValue=""
+                                        defaultValue={this.state.data_location}
                                         onBlur={this.handleFieldChange}
                                         variant="outlined"
                                         fullWidth={true}

apps/model_catalog/src/ModelDetail.js
@@ -416,7 +416,7 @@ class ModelDetail extends React.Component {
                                 compareFlag={this.state.compareFlag}
                                 addModelCompare={this.addModelCompare}
                                 removeModelCompare={this.removeModelCompare}
-                            ></ModelDetailHeader>
+                            />
                         </Grid>
                         <Grid item xs={12}>
                             <AppBar position="static">

apps/model_catalog/src/ModelDetailHeader.js
@@ -3,6 +3,7 @@ import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import AddToQueueIcon from "@material-ui/icons/AddToQueue";
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import EditIcon from "@material-ui/icons/Edit";
 import LockIcon from "@material-ui/icons/Lock";
 import PublicIcon from "@material-ui/icons/Public";
@@ -12,6 +13,7 @@ import React from "react";
 import ContextMain from "./ContextMain";
 import ErrorDialog from "./ErrorDialog";
 import ModelEditForm from "./ModelEditForm";
+import ModelAddForm from "./ModelAddForm";
 import Theme from "./theme";
 import {
     copyToClipboard,
@@ -115,17 +117,24 @@ class ModelDetailHeader extends React.Component {
 
         this.state = {
             openEditForm: false,
+            openDuplicateForm: false,
             errorEditModel: null,
+            errorDuplicateModel: null,
         };
-        this.handleEditModelFormClose =
-            this.handleEditModelFormClose.bind(this);
+        this.handleEditModelFormClose = this.handleEditModelFormClose.bind(this);
+        this.handleDuplicateModelFormClose = this.handleDuplicateModelFormClose.bind(this);
         this.handleEditClick = this.handleEditClick.bind(this);
-        this.handleErrorEditDialogClose =
-            this.handleErrorEditDialogClose.bind(this);
+        this.handleDuplicateClick = this.handleDuplicateClick.bind(this);
+        this.handleErrorEditDialogClose = this.handleErrorEditDialogClose.bind(this);
+        this.handleErrorDuplicateDialogClose = this.handleErrorDuplicateDialogClose.bind(this);
     }
 
     handleErrorEditDialogClose() {
         this.setState({ errorEditModel: null });
+    }
+
+    handleErrorDuplicateDialogClose() {
+        this.setState({ errorDuplicateModel: null });
     }
 
     handleEditModelFormClose(model) {
@@ -143,9 +152,30 @@ class ModelDetailHeader extends React.Component {
         }
     }
 
+    handleDuplicateModelFormClose(model) {
+        console.log("close duplicate");
+
+        this.setState({ openDuplicateForm: false });
+        if (model) {
+            this.props.updateCurrentModelData(model);
+            showNotification(
+                this.props.enqueueSnackbar,
+                this.props.closeSnackbar,
+                "Model duplicated!",
+                "success"
+            );
+        }
+    }
+
     handleEditClick() {
         this.setState({
             openEditForm: true,
+        });
+    }
+
+    handleDuplicateClick() {
+        this.setState({
+            openDuplicateForm: true,
         });
     }
 
@@ -163,6 +193,18 @@ class ModelDetailHeader extends React.Component {
                 />
             );
         }
+        if (this.state.errorDuplicateModel) {
+            errorMessage = (
+                <ErrorDialog
+                    open={Boolean(this.state.errorDuplicateModel)}
+                    handleErrorDialogClose={this.handleErrorDuplicateDialogClose}
+                    error={
+                        this.state.errorDuplicateModel.message ||
+                        this.state.errorDuplicateModel
+                    }
+                />
+            );
+        }
 
         let editForm = "";
         if (this.state.openEditForm) {
@@ -171,6 +213,17 @@ class ModelDetailHeader extends React.Component {
                     open={this.state.openEditForm}
                     onClose={this.handleEditModelFormClose}
                     modelData={this.props.modelData}
+                />
+            );
+        }
+
+        let duplicateForm = "";
+        if (this.state.openDuplicateForm) {
+            duplicateForm = (
+                <ModelAddForm
+                    open={this.state.openDuplicateForm}
+                    onClose={this.handleDuplicateModelFormClose}
+                    duplicateData={this.props.modelData}
                 />
             );
         }
@@ -198,6 +251,18 @@ class ModelDetailHeader extends React.Component {
                             canEdit={this.props.canEdit}
                             handleEditClick={this.handleEditClick}
                         />
+                        <Tooltip placement="top" title="Duplicate Model">
+                            <IconButton
+                                aria-label="duplicate model"
+                                onClick={() => this.handleDuplicateClick()}
+                                style={{
+                                    backgroundColor: Theme.buttonPrimary,
+                                    marginLeft: 10,
+                                }}
+                            >
+                                <FileCopyIcon />
+                            </IconButton>
+                        </Tooltip>
                         <CompareIcon
                             compareFlag={this.props.compareFlag}
                             addModelCompare={this.props.addModelCompare}
@@ -275,6 +340,7 @@ class ModelDetailHeader extends React.Component {
                 {/* optional image goes here */}
                 {/* </Grid> */}
                 <div>{editForm}</div>
+                <div>{duplicateForm}</div>
                 <div>{errorMessage}</div>
             </React.Fragment>
         );

apps/model_catalog/src/TestDetail.js
@@ -388,7 +388,7 @@ class TestDetail extends React.Component {
                                 compareFlag={this.state.compareFlag}
                                 addTestCompare={this.addTestCompare}
                                 removeTestCompare={this.removeTestCompare}
-                            ></TestDetailHeader>
+                            />
                         </Grid>
                         <Grid item xs={12}>
                             <AppBar position="static">

apps/model_catalog/src/TestDetailHeader.js
@@ -3,6 +3,7 @@ import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import AddToQueueIcon from "@material-ui/icons/AddToQueue";
+import FileCopyIcon from '@material-ui/icons/FileCopy';
 import EditIcon from "@material-ui/icons/Edit";
 import RemoveFromQueueIcon from "@material-ui/icons/RemoveFromQueue";
 import { withSnackbar } from "notistack";
@@ -10,6 +11,7 @@ import React from "react";
 import ContextMain from "./ContextMain";
 import ErrorDialog from "./ErrorDialog";
 import TestEditForm from "./TestEditForm";
+import TestAddForm from "./TestAddForm";
 import Theme from "./theme";
 import {
     copyToClipboard,
@@ -76,16 +78,24 @@ class TestDetailHeader extends React.Component {
 
         this.state = {
             openEditForm: false,
+            openDuplicateForm: false,
             errorEditTest: null,
+            errorDuplicateTest: null
         };
         this.handleEditTestFormClose = this.handleEditTestFormClose.bind(this);
+        this.handleDuplicateTestFormClose = this.handleDuplicateTestFormClose.bind(this);
         this.handleEditClick = this.handleEditClick.bind(this);
-        this.handleErrorEditDialogClose =
-            this.handleErrorEditDialogClose.bind(this);
+        this.handleDuplicateClick = this.handleDuplicateClick.bind(this);
+        this.handleErrorEditDialogClose = this.handleErrorEditDialogClose.bind(this);
+        this.handleErrorDuplicateDialogClose = this.handleErrorDuplicateDialogClose.bind(this);
     }
 
     handleErrorEditDialogClose() {
         this.setState({ errorEditTest: null });
+    }
+
+    handleErrorDuplicateDialogClose() {
+        this.setState({ errorDuplicateTest: null });
     }
 
     handleEditTestFormClose(test) {
@@ -103,9 +113,30 @@ class TestDetailHeader extends React.Component {
         }
     }
 
+    handleDuplicateTestFormClose(test) {
+        console.log("close duplicate");
+
+        this.setState({ openDuplicateForm: false });
+        if (test) {
+            this.props.updateCurrentTestData(test);
+            showNotification(
+                this.props.enqueueSnackbar,
+                this.props.closeSnackbar,
+                "Test duplicated!",
+                "success"
+            );
+        }
+    }
+
     handleEditClick() {
         this.setState({
             openEditForm: true,
+        });
+    }
+
+    handleDuplicateClick() {
+        this.setState({
+            openDuplicateForm: true,
         });
     }
 
@@ -123,6 +154,18 @@ class TestDetailHeader extends React.Component {
                 />
             );
         }
+        if (this.state.errorDuplicateTest) {
+            errorMessage = (
+                <ErrorDialog
+                    open={Boolean(this.state.errorDuplicateTest)}
+                    handleErrorDialogClose={this.handleErrorDuplicateDialogClose}
+                    error={
+                        this.state.errorDuplicateTest.message ||
+                        this.state.errorDuplicateTest
+                    }
+                />
+            );
+        }
 
         let editForm = "";
         if (this.state.openEditForm) {
@@ -131,6 +174,17 @@ class TestDetailHeader extends React.Component {
                     open={this.state.openEditForm}
                     onClose={this.handleEditTestFormClose}
                     testData={this.props.testData}
+                />
+            );
+        }
+
+        let duplicateForm = "";
+        if (this.state.openDuplicateForm) {
+            duplicateForm = (
+                <TestAddForm
+                    open={this.state.openDuplicateForm}
+                    onClose={this.handleDuplicateTestFormClose}
+                    duplicateData={this.props.testData}
                 />
             );
         }
@@ -163,6 +217,18 @@ class TestDetailHeader extends React.Component {
                                 }}
                             >
                                 <EditIcon />
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip placement="top" title="Duplicate Test">
+                            <IconButton
+                                aria-label="duplicate test"
+                                onClick={() => this.handleDuplicateClick()}
+                                style={{
+                                    backgroundColor: Theme.buttonPrimary,
+                                    marginLeft: 10,
+                                }}
+                            >
+                                <FileCopyIcon />
                             </IconButton>
                         </Tooltip>
                         <CompareIcon
@@ -249,6 +315,7 @@ class TestDetailHeader extends React.Component {
                 {/* optional image goes here */}
                 {/* </Grid> */}
                 <div>{editForm}</div>
+                <div>{duplicateForm}</div>
                 <div>{errorMessage}</div>
             </React.Fragment>
         );

apps/model_catalog/src/ValidationFramework.js
@@ -309,6 +309,7 @@ class ValidationFramework extends React.Component {
             modelData: updatedModelData,
             currentModel: updatedModel,
         });
+        updateHash("model_id." + updatedModel.id);
     }
 
     handleAddModelInstance(newModelInstance) {
@@ -365,6 +366,7 @@ class ValidationFramework extends React.Component {
             testData: updatedTestData,
             currentTest: updatedTest,
         });
+        updateHash("test_id." + updatedTest.id);
     }
 
     handleAddTestInstance(newTestInstance) {

commit 7438c4eb75c733879dfd4a27119e3757b50d841f

fix bug when adding new model or test from table
(probably introduced in 90cf39d)

apps/model_catalog/src/ModelAddForm.js
@@ -49,34 +49,39 @@ export default class ModelAddForm extends React.Component {
         const [validFilterValuesContext] = this.context.validFilterValues;
         const [filtersContext] = this.context.filters;
 
+        let sourceData = {instances: [{}]}
+        if (this.props.duplicateData) {
+            sourceData = this.props.duplicateData;
+        }
+
         this.state = {
             // NOTE: cannot use nested state object owing to performance issues:
             // See: https://dev.to/walecloud/updating-react-nested-state-properties-ga6
             errorAddModel: null,
             isAliasNotUnique: true,
             aliasLoading: false,
-            name: this.props.duplicateData.name || "",
-            alias: this.props.duplicateData.alias || "",
-            author: this.props.duplicateData.author || [],
-            owner: this.props.duplicateData.owner || [],
-            private: this.props.duplicateData.private || false,
-            project_id: this.props.duplicateData.project_id || "",
-            description: this.props.duplicateData.description || "",
-            species: this.props.duplicateData.species || "",
-            brain_region: this.props.duplicateData.brain_region || "",
-            cell_type: this.props.duplicateData.cell_type || "",
-            model_scope: this.props.duplicateData.model_scope || "",
-            abstraction_level: this.props.duplicateData.abstraction_level || "",
-            organization: this.props.duplicateData.organization || "",
+            name: sourceData.name || "",
+            alias: sourceData.alias || "",
+            author: sourceData.author || [],
+            owner: sourceData.owner || [],
+            private: sourceData.private || false,
+            project_id: sourceData.project_id || "",
+            description: sourceData.description || "",
+            species: sourceData.species || "",
+            brain_region: sourceData.brain_region || "",
+            cell_type: sourceData.cell_type || "",
+            model_scope: sourceData.model_scope || "",
+            abstraction_level: sourceData.abstraction_level || "",
+            organization: sourceData.organization || "",
             instances: [
                 {
-                    version: this.props.duplicateData.instances[0].version || "",
-                    description: this.props.duplicateData.instances[0].description || "",
-                    parameters: this.props.duplicateData.instances[0].parameters || "",
-                    morphology: this.props.duplicateData.instances[0].morphology || "",
-                    source: this.props.duplicateData.instances[0].source || "",
-                    code_format: this.props.duplicateData.instances[0].code_format || "",
-                    license: this.props.duplicateData.instances[0].license || "",
+                    version: sourceData.instances[0].version || "",
+                    description: sourceData.instances[0].description || "",
+                    parameters: sourceData.instances[0].parameters || "",
+                    morphology: sourceData.instances[0].morphology || "",
+                    source: sourceData.instances[0].source || "",
+                    code_format: sourceData.instances[0].code_format || "",
+                    license: sourceData.instances[0].license || "",
                 },
             ],
             auth: authContext,
@@ -85,6 +90,7 @@ export default class ModelAddForm extends React.Component {
             loading: false,
             projects: [],
         };
+
         this.getProjectList();
         this.handleErrorAddDialogClose =
             this.handleErrorAddDialogClose.bind(this);
@@ -131,6 +137,8 @@ export default class ModelAddForm extends React.Component {
                 this.setState({
                     projects: editableProjects,
                 });
+                console.log("Setting projects");
+                console.log(editableProjects);
             })
             .catch((err) => {
                 console.log("Error: ", err.message);

apps/model_catalog/src/TestAddForm.js
@@ -47,32 +47,37 @@ export default class TestAddForm extends React.Component {
         const [validFilterValuesContext] = this.context.validFilterValues;
         const [filtersContext] = this.context.filters;
 
+        let sourceData = {instances: [{}]}
+        if (this.props.duplicateData) {
+            sourceData = this.props.duplicateData;
+        }
+
         this.state = {
             // NOTE: cannot use nested state object owing to performance issues:
             // See: https://dev.to/walecloud/updating-react-nested-state-properties-ga6
             errorAddTest: null,
             isAliasNotUnique: true,
             aliasLoading: false,
-            name: this.props.duplicateData.name || "",
-            alias: this.props.duplicateData.alias || "",
-            author: this.props.duplicateData.author || [],
-            description: this.props.duplicateData.description || "",
-            data_location: this.props.duplicateData.data_location || [],
-            data_type: this.props.duplicateData.data_type || "",
-            species: this.props.duplicateData.species || "",
-            brain_region: this.props.duplicateData.brain_region || "",
-            cell_type: this.props.duplicateData.cell_type || "",
-            test_type: this.props.duplicateData.test_type || "",
-            score_type: this.props.duplicateData.score_type || "",
-            recording_modality: this.props.duplicateData.recording_modality || "",
-            implementation_status: this.props.duplicateData.implementation_status || "",
+            name: sourceData.name || "",
+            alias: sourceData.alias || "",
+            author: sourceData.author || [],
+            description: sourceData.description || "",
+            data_location: sourceData.data_location || [],
+            data_type: sourceData.data_type || "",
+            species: sourceData.species || "",
+            brain_region: sourceData.brain_region || "",
+            cell_type: sourceData.cell_type || "",
+            test_type: sourceData.test_type || "",
+            score_type: sourceData.score_type || "",
+            recording_modality: sourceData.recording_modality || "",
+            implementation_status: sourceData.implementation_status || "",
             instances: [
                 {
-                    version: this.props.duplicateData.instances[0].version || "",
-                    repository: this.props.duplicateData.instances[0].repository || "",
-                    path: this.props.duplicateData.instances[0].path || "",
-                    description: this.props.duplicateData.instances[0].description || "",
-                    parameters: this.props.duplicateData.instances[0].parameters || "",
+                    version: sourceData.instances[0].version || "",
+                    repository: sourceData.instances[0].repository || "",
+                    path: sourceData.instances[0].path || "",
+                    description: sourceData.instances[0].description || "",
+                    parameters: sourceData.instances[0].parameters || "",
                 },
             ],
             auth: authContext,